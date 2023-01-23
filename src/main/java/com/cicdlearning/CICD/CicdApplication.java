48f95c13b74ad1f3ee9774af8ec64a6c6ac81a8f
yalladri2512 <kurubayalladri778031@gmail.com>
2023-01-23 15:09:30 +0530

    mysecondcommit

--- src/main/java/com/cicdlearning/CICD/CicdApplication.java
+++ src/main/java/com/cicdlearning/CICD/CicdApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 	}
